32a8d3afe056d08af3654faabefb41ed8691922c
asvinod <asvinod@ucsc.edu>
2025-01-11 02:03:39 +0000

    Experimenting with Figma code

--- nutrition-app/app.py
+++ nutrition-app/app.py
@@ -2,13 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/hi")
-def hi():
-    return "<p>Ok</p>"
-
-@app.route("/")
-def hello_world():
-    message = "<p>Hello World!</p>"
-    link = '<p><a href="/hi">F*ck you!</a></p>'
-
-    return message + link
+@app.route('/')
+def index():
+    return render_template('index.html')
